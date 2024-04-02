f96a2072f65427c8596fc8b95c6fdee96190d1b2
opyshna <129679887+opyshna@users.noreply.github.com>
2024-04-02 21:31:07 +0300

    Update visualization.py

--- visualization.py
+++ visualization.py
@@ -43,29 +43,23 @@ def print_bar_chart(name, data, metric, normalize=True, maxnum=None, minval=None
     print()
 
 summary_c = summary[summary["model"].isin(cumulative_m)]
-print_bar_chart("Likelihood, 1+", summary_c, "l")
-print_bar_chart("Evidence, 1+", summary_c, "z")
-print_bar_chart("Likelihood, top 15", summary, "l", maxnum=15, minval=0.0001)
-print_bar_chart("Evidence, top 15", summary, "z", maxnum=15, minval=0.0001)
-print_bar_chart("AIC, top 15", summary, "aicscore", maxnum=15, minval=0.0001)
-
 summary_s = {}
 for i in range(len(summary)):
     d = summary.iloc[i]
     key = d["type"]
-    if key not in summary_s: summary_s[key] = [0, 0]
+    if key not in summary_s: summary_s[key] = [0, 0, 0]
     summary_s[key][0] += d["l"]
     summary_s[key][1] += d["z"]
-summary_s = pd.DataFrame([["Σ", i[0], i[1][0], i[1][1]] for i in summary_s.items()], columns=["model", "type", "l", "z"])
+    summary_s[key][2] += d["aicscore"]
+summary_s = pd.DataFrame([["Σ", i[0], i[1][0], i[1][1], i[1][2]] for i in summary_s.items()], columns=["model", "type", "l", "z", "aicscore"])
 
 print_bar_chart("Likelihood, Σ", summary_s, "l", printmodel=False)
 print_bar_chart("Evidence, Σ", summary_s, "z", printmodel=False)
-print_bar_chart("Likelihood, Σ1", summary_s[summary_s["type"].isin(["SN Ia", "SN Ib", "SN Ic", "SN Ic-BL", "SN IIL", "SN IIP", "SN IIn"])], "l", printmodel=False)
-summary_s2 = {"SN I a  ": ["SN Ia"], "SN I b/c": ["SN Ib", "SN Ic", "SN Ib/c"], "SN IIL/P": ["SN IIL/P", "SN IIP", "SN IIL"], "SN IIn  ": ["SN IIn"]}
+print_bar_chart("AIC, Σ", summary_s, "aicscore", printmodel=False)
+
 summary_s3 = {"SN Ia  ": ["SN Ia"], "SN Ib/c": ["SN Ib", "SN Ic", "SN Ib/c"], "SN II  ": ["SN IIL/P", "SN IIP", "SN IIL", "SN IIn", "SN II", "SN II-pec"]}
-summary_s2 = {i[0]: [sum(summary_s[summary_s["type"].isin(i[1])]["l"]), sum(summary_s[summary_s["type"].isin(i[1])]["z"])] for i in summary_s2.items()}
-summary_s3 = {i[0]: [sum(summary_s[summary_s["type"].isin(i[1])]["l"]), sum(summary_s[summary_s["type"].isin(i[1])]["z"])] for i in summary_s3.items()}
-summary_s2 = pd.DataFrame([["Σ", i[0], i[1][0], i[1][1]] for i in summary_s2.items()], columns=["model", "type", "l", "z"])
-summary_s3 = pd.DataFrame([["Σ", i[0], i[1][0], i[1][1]] for i in summary_s3.items()], columns=["model", "type", "l", "z"])
-print_bar_chart("Likelihood, Σ2", summary_s2, "l", printmodel=False)
+summary_s3 = {i[0]: [sum(summary_s[summary_s["type"].isin(i[1])]["l"]), sum(summary_s[summary_s["type"].isin(i[1])]["z"]), sum(summary_s[summary_s["type"].isin(i[1])]["aicscore"])] for i in summary_s3.items()}
+summary_s3 = pd.DataFrame([["Σ", i[0], i[1][0], i[1][1], i[1][2]] for i in summary_s3.items()], columns=["model", "type", "l", "z", "aicscore"])
 print_bar_chart("Likelihood, Σ3", summary_s3, "l", printmodel=False)
+print_bar_chart("Evidence, Σ3", summary_s3, "z", printmodel=False)
+print_bar_chart("AIC, Σ3", summary_s3, "aicscore", printmodel=False)
